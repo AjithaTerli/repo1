cfe95bd1afad5281c49e88f1defd5c79e9707632
AjithaTerli <ajithaterli@gmail.com>
2025-03-11 10:32:15 +0530

    Update two.java

--- two.java
+++ two.java
@@ -2,5 +2,7 @@ public static void main(String args[]){
 
     System.out.println("welcome to Devops");
     System.out.println("welcome to sree Dattha collge");
+     System.out.println("welcome to SDES");
+   
    
 }
